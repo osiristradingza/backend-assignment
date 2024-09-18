b7b3c8edd03bf4fff585880ab42398fefcd0846d
Goldin Baloyi <goldin.baloyi@gitechnologies.co.za>
2024-09-18 14:10:56 +0200

    add input validations

--- src/OT.Assessment.Model/AddCasinoWagerRequest.cs
+++ src/OT.Assessment.Model/AddCasinoWagerRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,23 @@ namespace OT.Assessment.Model
 {
     public class AddCasinoWagerRequest
     {
+        [Required]
         public string Theme { get; set; }
+        [Required]
         public string ProviderName { get; set; }
+        [Required]
         public string GameName { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string TransactionType { get; set; }
+        [Required]
+        [Range(1, 9999999999999999.99)]
         public decimal Amount { get; set; }
+        [Required]
         public string CountryCode { get; set; }
+        [Required]
+        [Range(1,int.MaxValue)]
         public int NumberOfBets { get; set; }
 
     }
